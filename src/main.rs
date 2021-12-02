155a3c3bf97ba95d92a6ea9319d5cfa34b72becf
Timothy Iles <admin@windowslogic.co.uk>
2021-12-02 10:59:46 +0000

    Removed old code.

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,13 @@ use std::io::{self, BufRead};
 use std::path::Path;
 use std::io::prelude::*;
 
-fn main() { //-> std::io::Result<()> {
+fn main() {
 
     if let Ok(lines) = read_lines("src/names.txt") {
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
             if let Ok(names) = line {
                 println!("{}", names);
-
-
-
-    //let mut index = 0;
-    //let mut file = File::open("src/names.txt")?;
-    //let mut contents = String::new();
-    //file.read_to_string(&mut contents)?;
-    //println!("{}", contents);
 }
 }
 }
