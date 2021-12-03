ee656b346a25cf66638dcce4923c3fdf35cc5960
Timothy Iles <admin@windowslogic.co.uk>
2021-12-03 13:03:56 +0000

    Added broken argument acceptor.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::env;
 
 fn main() {
 
-    let mut index = 1;
+    let mut index = 0;
+    let args: Vec<String> = env::args().collect();
+    let arg = &args[1];
+    let count: i32 = match arg.parse() {
+        Ok(n) => {
+            n
+        },
+        Err(_) => {
+                    eprintln!("error: argument not an integer");
+                    return;
+        }
+        };
+        
+        println!("{}", count);
     
+    while index < count {
     if let Ok(lines) = read_lines("src/names.txt") {
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
             if let Ok(names) = line {
-                println!("{}. {}", index, names);
+                println!("{}", names);
                 
                 index += 1;
+}
 }
 }
 }
